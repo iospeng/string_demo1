e3121ab72b12e3149d23758293c1e3d540defc86
iospeng <iospeng@163.com>
2016-04-21 15:37:36 +0800

    string_demo

--- string_demo1/ViewController.m
+++ string_demo1/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *str = @"hello world";
+    NSString *str1 = [str stringByReplacingOccurrencesOfString:@" " withString:@" asdf "];
+    NSLog(@"%@",str1);
 }
 
 - (void)didReceiveMemoryWarning {
