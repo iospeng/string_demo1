a3323be88e503f1dd7fe92404c24edf140c00f99
iospeng <iospeng@163.com>
2016-04-21 15:27:10 +0800

    Initial Commit

---
+++ string_demo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  string_demo1
+//
+//  Created by 宋伦超 on 16/4/21.
+//  Copyright © 2016年 Mr.Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ string_demo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  string_demo1
+//
+//  Created by 宋伦超 on 16/4/21.
+//  Copyright © 2016年 Mr.Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
